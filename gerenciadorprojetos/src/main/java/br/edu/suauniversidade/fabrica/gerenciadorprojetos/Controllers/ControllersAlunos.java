efffed85da7ea9624e83e2ff8e367738b32d34c8
ismailergregorio <ismailergregorio6@gmail.com>
2025-07-28 23:02:21 -0300

    .ajuste

--- gerenciadorprojetos/src/main/java/br/edu/suauniversidade/fabrica/gerenciadorprojetos/Controllers/ControllersAlunos.java
+++ gerenciadorprojetos/src/main/java/br/edu/suauniversidade/fabrica/gerenciadorprojetos/Controllers/ControllersAlunos.java
@@ -1,7 +1,9 @@
 package br.edu.suauniversidade.fabrica.gerenciadorprojetos.Controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.suauniversidade.fabrica.gerenciadorprojetos.DTO.dtoAlunosPost;
 import br.edu.suauniversidade.fabrica.gerenciadorprojetos.DTO.dtoAlunosRespost;
+import br.edu.suauniversidade.fabrica.gerenciadorprojetos.DTO.dtoProjetos;
 import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Model.ClassAlunos;
+import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Model.ClassProjetos;
 import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Repository.RepositoryAlunos;
+import jakarta.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/alunos")
@@ -26,7 +31,7 @@ public class ControllersAlunos {
 
     @PostMapping("/addalunos")
     public ResponseEntity<dtoAlunosRespost> postAlunos(@RequestBody dtoAlunosPost dto) {
-        //TODO: process POST request
+        // TODO: process POST request
         ClassAlunos aluno = new ClassAlunos();
 
         aluno.setRa(dto.getRa());
@@ -50,10 +55,28 @@ public class ControllersAlunos {
 
     @GetMapping("/alunos")
     public List<dtoAlunosRespost> getAlunos() {
-        List<ClassAlunos> alunos =  repositoryAlunos.findAll();
+        List<ClassAlunos> alunos = repositoryAlunos.findAll();
         return alunos.stream().map(dtoAlunosRespost::new).toList();
     }
-    
-    
-    
+
+    @GetMapping("/aluno{ra}")
+    public ResponseEntity<dtoAlunosRespost> getAluno(@PathVariable String ra) {
+        Optional<ClassAlunos> alunoOptional = repositoryAlunos.findByNome(ra);
+
+        if (alunoOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        ClassAlunos projeto = alunoOptional.get();
+
+        dtoAlunosRespost dtoSelecionado = new dtoAlunosRespost();
+
+        dtoSelecionado.getNome();
+        dtoSelecionado.getEmailInstitucional();
+        dtoSelecionado.getCurso();
+        dtoSelecionado.getProjetoSelecionado();
+        dtoSelecionado.getMotivoDaInscricao();
+        return ResponseEntity.ok(ra);
+    }
+
 }
--- gerenciadorprojetos/src/main/java/br/edu/suauniversidade/fabrica/gerenciadorprojetos/Repository/RepositoryAlunos.java
+++ gerenciadorprojetos/src/main/java/br/edu/suauniversidade/fabrica/gerenciadorprojetos/Repository/RepositoryAlunos.java
@@ -1,11 +1,13 @@
 package br.edu.suauniversidade.fabrica.gerenciadorprojetos.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Model.ClassAlunos;
 
 @Repository
-public interface RepositoryAlunos extends JpaRepository<ClassAlunos,Long>{
- 
+public interface RepositoryAlunos extends JpaRepository<ClassAlunos,String>{
+ Optional<ClassAlunos> findByNome(String ra);
 }
