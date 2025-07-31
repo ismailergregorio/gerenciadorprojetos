c653cf659a5884ae1a1d71b2b61fb0bdeef51607
ismailergregorio <projetointegradorextensao@gmail.com>
2025-07-31 12:54:21 -0300

    .ajuste

--- gerenciadorprojetos/src/main/java/br/edu/suauniversidade/fabrica/gerenciadorprojetos/Controllers/ControllersAlunos.java
+++ gerenciadorprojetos/src/main/java/br/edu/suauniversidade/fabrica/gerenciadorprojetos/Controllers/ControllersAlunos.java
@@ -15,6 +15,7 @@ import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Repository.RepositoryA
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/alunos")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ControllersAlunos {
     @Autowired
     RepositoryAlunos repositoryAlunos;
--- gerenciadorprojetos/src/main/java/br/edu/suauniversidade/fabrica/gerenciadorprojetos/Controllers/ControllesProjetos.java
+++ gerenciadorprojetos/src/main/java/br/edu/suauniversidade/fabrica/gerenciadorprojetos/Controllers/ControllesProjetos.java
@@ -14,13 +14,14 @@ import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Repository.RepositoryP
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/projetos")
-
+@CrossOrigin(origins = "http://localhost:3000")
 public class ControllesProjetos {
  @Autowired
  RepositoryProjetos repositorioProjetos;
